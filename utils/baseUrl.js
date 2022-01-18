45ec801426b27b075e480649e5beca9be2db8c3c
shehzadfalcon <shehzad.falcon@gmail.com>
2022-01-18 23:43:04 +0500

    edit profile

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://westendnodejs.herokuapp.com"
+    ? "https://volksanddavid.herokuapp.com"
     : "http://localhost:5000"
 
 export default baseUrl
